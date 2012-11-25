dc78fb2c40a92291a5080596910f3e6afec43425
praveenrengarajan <rengaraj.praveen@gmail.com>
2012-11-25 22:08:43 +0530

    first bitonalconvertor V1

---
+++ bitonalconverter/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1,System.ComponentModel.ISupportInitialize).BeginInit
+        CType(Me.PictureBox2,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.SuspendLayout
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(499, 414)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = false
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(597, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(499, 414)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = false
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(199, 433)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Open"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(809, 433)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Save"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1108, 475)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "RGB to Bitonal Converter"
+        CType(Me.PictureBox1,System.ComponentModel.ISupportInitialize).EndInit
+        CType(Me.PictureBox2,System.ComponentModel.ISupportInitialize).EndInit
+        Me.ResumeLayout(false)
+
+End Sub
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+End Class
---
+++ bitonalconverter/Form1.vb
@@ -0,0 +1,112 @@
+﻿Imports System.Drawing.Imaging
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+    Dim optimgname As Bitmap
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        OpenFileDialog1.ShowDialog()
+        Dim filname As String = OpenFileDialog1.FileName
+        Dim fname As Bitmap = New Bitmap(filname)
+        PictureBox1.Image = fname
+        optimgname = ConvertToBitonal(fname)
+        PictureBox2.Image = optimgname
+    End Sub
+
+
+    Public Function ConvertToBitonal(ByRef original As Bitmap) As Bitmap
+        Dim source As Bitmap = Nothing
+
+        ' If original bitmap is not already in 32 BPP, ARGB format, then convert
+        If original.PixelFormat <> PixelFormat.Format32bppArgb Then
+            source = New Bitmap(original.Width, original.Height, PixelFormat.Format32bppArgb)
+            source.SetResolution(original.HorizontalResolution, original.VerticalResolution)
+            Using g As Graphics = Graphics.FromImage(source)
+                g.DrawImageUnscaled(original, 0, 0)
+            End Using
+        Else
+            source = original
+        End If
+
+        ' Lock source bitmap in memory
+        Dim sourceData As BitmapData = source.LockBits(New Rectangle(0, 0, source.Width, source.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb)
+
+        ' Copy image data to binary array
+        Dim imageSize As Integer = sourceData.Stride * sourceData.Height
+        Dim sourceBuffer(imageSize - 1) As Byte
+        Marshal.Copy(sourceData.Scan0, sourceBuffer, 0, imageSize)
+
+        ' Unlock source bitmap
+        source.UnlockBits(sourceData)
+
+        ' Create destination bitmap
+        Dim destination As Bitmap = New Bitmap(source.Width, source.Height, PixelFormat.Format1bppIndexed)
+        destination.SetResolution(original.HorizontalResolution, original.VerticalResolution)
+
+        ' Lock destination bitmap in memory
+        Dim destinationData As BitmapData = destination.LockBits(New Rectangle(0, 0, destination.Width, destination.Height), ImageLockMode.WriteOnly, PixelFormat.Format1bppIndexed)
+
+        ' Create destination buffer
+        imageSize = destinationData.Stride * destinationData.Height
+
+        Dim destinationBuffer(imageSize - 1) As Byte
+        Dim sourceIndex As Integer = 0
+        Dim destinationIndex As Integer = 0
+        Dim pixelTotal As Integer = 0
+        Dim destinationValue As Byte = 0
+        Dim pixelValue As Integer = 128
+        Dim height As Integer = source.Height
+        Dim width As Integer = source.Width
+        Dim threshold As Integer = 500
+
+        ' Iterate lines
+        For y As Integer = 0 To height - 1
+
+            sourceIndex = y * sourceData.Stride
+            destinationIndex = y * destinationData.Stride
+            destinationValue = 0
+            pixelValue = 128
+
+            ' Iterate pixels
+            For x As Integer = 0 To width - 1
+                ' Compute pixel brightness (i.e. total of Red, Green, and Blue values) - Thanks murx
+                ' B G R
+                pixelTotal = CInt(sourceBuffer(sourceIndex)) + CInt(sourceBuffer(sourceIndex + 1)) + CInt(sourceBuffer(sourceIndex + 2))
+                If (pixelTotal > threshold) Then
+                    destinationValue += CType(pixelValue, Byte)
+                End If
+                If pixelValue = 1 Then
+                    destinationBuffer(destinationIndex) = destinationValue
+                    destinationIndex += 1
+                    destinationValue = 0
+                    pixelValue = 128
+                Else
+                    pixelValue >>= 1
+                End If
+                sourceIndex += 4
+            Next
+            If pixelValue <> 128 Then
+                destinationBuffer(destinationIndex) = destinationValue
+            End If
+        Next
+
+        ' Copy binary image data to destination bitmap
+        Marshal.Copy(destinationBuffer, 0, destinationData.Scan0, imageSize)
+
+        ' Unlock destination bitmap
+        destination.UnlockBits(destinationData)
+
+        ' Dispose of source if not originally supplied bitmap
+        If Not Object.ReferenceEquals(source, original) Then
+            source.Dispose()
+        End If
+
+        Return destination
+    End Function
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        SaveFileDialog1.ShowDialog()
+        Dim optfile As String = SaveFileDialog1.FileName
+        Dim frmt As ImageFormat = ImageFormat.Tiff
+        optimgname.Save(optfile, frmt)
+    End Sub
+End Class
